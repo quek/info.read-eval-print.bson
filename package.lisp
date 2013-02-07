f2fed1447a98401182d78daf854672b23dedeaec
Yoshinori Tahara <read.eval.print@gmail.com>
2013-02-07 20:42:17 +0900

    export +type-.*+

--- package.lisp
+++ package.lisp
@@ -12,5 +12,27 @@
            #:+bson-null+
            #:+bson-empty-array+
            #:+bson-min-key+
-           #:+bson-max-key+))
+           #:+bson-max-key+
 
+           #:+type-end-of-bson+
+           #:+type-double+
+           #:+type-string+
+           #:+type-document+
+           #:+type-array+
+           #:+type-binary+
+           #:+type-undefined+
+           #:+type-object-id+
+           #:+type-false+
+           #:+type-true+
+           #:+type-datetime+
+           #:+type-null+
+           #:+type-regex+
+           #:+type-db-pointer+
+           #:+type-java-script+
+           #:+type-symbol+
+           #:+type-code-w-s+
+           #:+type-int32+
+           #:+type-timestamp+
+           #:+type-int64+
+           #:+type-min-key+
+           #:+type-max-key+))
